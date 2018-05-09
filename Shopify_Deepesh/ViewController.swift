3ad0a08b7921610ef93d7e4f8e7ff4dc609012cb
Deepesh Mehta <deepeshhmehta@gmail.com>
2018-05-09 12:07:53 -0400

    data fetch completed... implication pending

--- Shopify_Deepesh/ViewController.swift
+++ Shopify_Deepesh/ViewController.swift
@@ -8,11 +8,46 @@
 
 import UIKit
 
+
 class ViewController: UITabBarController {
     var data : Int = 2
     override func viewDidLoad() {
         super.viewDidLoad()
-
+  
+        let urlPath: String = "https://shopicruit.myshopify.com/admin/orders.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6"
+        let url: NSURL = NSURL(string: urlPath)!
+        let request1: NSURLRequest = NSURLRequest(url: url as URL)
+        let response: AutoreleasingUnsafeMutablePointer<URLResponse?>? = nil
+        
+        
+        do{
+            
+            let dataVal = try NSURLConnection.sendSynchronousRequest(request1 as URLRequest, returning: response)
+            
+            print(response)
+            do {
+                if let jsonResult = try JSONSerialization.jsonObject(with: dataVal, options: []) as? NSDictionary {
+                    print("Synchronous\(jsonResult)")
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+            
+            
+            
+        }catch let error as NSError
+        {
+            print(error.localizedDescription)
+        }
+/*
+        Alamofire.request("https://shopicruit.myshopify.com/admin/orders.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6")
+            .responseJSON { (responseData) -> Void in
+                guard let responseJSON = responseData.result.value as? [String: Any],
+                    let results = responseJSON["data"] as? [[String: Any]]
+                    else {
+                        return
+                }
+*/
         // Do any additional setup after loading the view.
     }
 
